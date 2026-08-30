881c6f2235d596a45eb4045feffa65df5fcd0d4c
Jireh Kenneth-Usen <jirehkenneth2001@gmail.com>
2026-08-30 02:42:05 +0100

    Align rate limiter storage configuration

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -25,7 +25,10 @@ def create_app(config_override: dict = None):
     app.config.setdefault("LOG_MAX_BYTES", 512 * 1024)
     app.config.setdefault("LOG_BACKUPS", 5)
     app.config.setdefault("HONEY_POT_FIELD", "hp_1aa74582")
-    app.config.setdefault("LIMITER_STORAGE_URI", "memory://")
+    app.config.setdefault(
+        "RATELIMIT_STORAGE_URI",
+        os.environ.get("RATELIMIT_STORAGE_URI", "memory://")
+    )
     app.config.setdefault("DEFAULT_RATE_LIMITS", ["120 per minute"])
 
     # Flask session secret:
